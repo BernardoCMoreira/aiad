9196ab59be34b489b970e9f2c2076bd0517eda88
Bernardo Santos <bernardosantos670@gmail.com>
2020-10-28 22:41:48 +0000

    ControlTower, Airplane and Runway agents now register in the DF at setup

--- src/com/aiad/agents/Airplane.java
+++ src/com/aiad/agents/Airplane.java
@@ -1,6 +1,10 @@
 package com.aiad.agents;
 
 import jade.core.Agent;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
 
 public class Airplane extends Agent {
     private int id,  waitTime = 0, timeToArrive;
@@ -22,6 +26,30 @@ public class Airplane extends Agent {
         this.timeToArrive = Integer.parseInt(splitMessage[2]);
         this.fuelRemaining = Integer.parseInt(splitMessage[3]);
         this.landed = splitMessage[4].equals("true");
+    }
+
+    protected void setup() {
+        DFAgentDescription description = new DFAgentDescription();
+        description.setName(getAID());
+        ServiceDescription service = new ServiceDescription();
+        service.setType("airplane");
+        description.addServices(service);
+
+        try {
+            DFService.register(this, description);
+        }
+        catch(FIPAException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void takeDown() {
+        try {
+            DFService.deregister(this);
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public int getID(){
--- src/com/aiad/agents/ControlTower.java
+++ src/com/aiad/agents/ControlTower.java
@@ -1,5 +1,9 @@
 package com.aiad.agents;
 import jade.core.*;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
 
 import java.util.ArrayList;
 
@@ -12,5 +16,28 @@ public class ControlTower extends Agent {
         runwayState = new ArrayList<>();
     }
 
+    protected void setup() {
+        DFAgentDescription description = new DFAgentDescription();
+        description.setName(getAID());
+        ServiceDescription service = new ServiceDescription();
+        service.setType("control_tower");
+        description.addServices(service);
+
+        try {
+            DFService.register(this, description);
+        }
+        catch(FIPAException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void takeDown() {
+        try {
+            DFService.deregister(this);
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 }
