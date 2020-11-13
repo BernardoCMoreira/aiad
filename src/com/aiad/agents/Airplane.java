bd0d17ac9b961fa7b80ced2c67be145274913ba9
Bernardo Santos <bernardosantos670@gmail.com>
2020-11-13 14:57:16 +0000

    Refactored airplanes

--- src/com/aiad/agents/Airplane.java
+++ src/com/aiad/agents/Airplane.java
@@ -1,9 +1,20 @@
 package com.aiad.agents;
 
 import com.aiad.messages.AirplaneInform;
+import com.aiad.messages.ArrivingAirplaneRequest;
+import jade.core.AID;
 import jade.core.Agent;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
+import jade.domain.FIPANames;
 import jade.lang.acl.ACLMessage;
 import jade.lang.acl.UnreadableException;
+import jade.proto.AchieveREInitiator;
+
+import java.io.IOException;
+import java.util.Vector;
 
 public class Airplane extends Agent {
 
@@ -50,40 +61,86 @@ public class Airplane extends Agent {
         return landed;
     }
 
-    // Handlers
-    public void handleAgree(ACLMessage agree) {
-        System.out.println("I received an agree!");
-    }
+    @Override
+    protected void setup() {
+        DFAgentDescription description = new DFAgentDescription();
+        description.setName(getAID());
+        ServiceDescription service = new ServiceDescription();
+        service.setName("airplane");
+        service.setType("airplane");
+        description.addServices(service);
 
-    public void handleRefuse(ACLMessage refuse) {
-        System.out.println("I received a refuse!");
-    }
-
-    public void handleInform(ACLMessage inform) {
-        System.out.println("I received an inform!");
         try {
-            AirplaneInform content = (AirplaneInform) inform.getContentObject();
-            System.out.println(content.toString());
-        } catch (UnreadableException e) {
+            DFService.register(this, description);
+        }
+        catch(FIPAException e) {
+            e.printStackTrace();
+        }
+
+        // setup the request message
+        ACLMessage request = new ACLMessage(ACLMessage.REQUEST);
+        // TODO: change this content
+        ArrivingAirplaneRequest content = new ArrivingAirplaneRequest();
+        content.setId(id);
+        content.setEta(timeToArrive);
+        content.setAutonomy(fuelRemaining);
+        try {
+            request.setContentObject(content);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        request.addReceiver(new AID("tower", AID.ISLOCALNAME));
+        request.setProtocol(FIPANames.InteractionProtocol.FIPA_REQUEST);
+
+        addBehaviour(new AirplaneRequestInitiator(this, request));
+    }
+
+    protected void takeDown() {
+        try {
+            DFService.deregister(this);
+        }
+        catch(Exception e) {
             e.printStackTrace();
         }
     }
 
-    public void handleFailure(ACLMessage failure) {
-        System.out.println("I received a failure!");
-    }
+    class AirplaneRequestInitiator extends AchieveREInitiator {
 
-    public void handleMessagesReceived() {
-        ACLMessage msg = receive();
-        if (msg != null) {
-            switch (msg.getPerformative()) {
-                case ACLMessage.AGREE:
-                    handleAgree(msg);
-                    break;
-                case ACLMessage.INFORM:
-                    handleInform(msg);
-                    break;
+        public AirplaneRequestInitiator(Agent a, ACLMessage msg) {
+            super(a, msg);
+        }
+
+        @Override
+        protected Vector prepareRequests(ACLMessage request) {
+            Vector<ACLMessage> v = new Vector<ACLMessage>();
+            v.add(request);
+            return v;
+        }
+
+        @Override
+        protected void handleAgree(ACLMessage agree) {
+            System.out.println("Received an agree message.");
+        }
+
+        @Override
+        protected void handleRefuse(ACLMessage refuse) {
+            System.out.println("Received a refuse message.");
+        }
+
+        @Override
+        protected void handleInform(ACLMessage inform) {
+            System.out.println("Received an inform message.");
+            try {
+                AirplaneInform content = (AirplaneInform) inform.getContentObject();
+                System.out.println(content.toString());
+            } catch (UnreadableException e) {
+                e.printStackTrace();
             }
+        }
+
+        @Override
+        protected void handleFailure(ACLMessage failure) {
+            System.out.println("Received a failure message.");
         }
     }
 }
